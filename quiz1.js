a1a97403c13dd8eb3d09b108fcc22c86d7cb221d
max01218 <max2015gept@example.com>
2021-03-31 18:14:46 +0800

    fifth commit

--- quiz1.js
+++ quiz1.js
@@ -9,7 +9,7 @@ function change() {
       header.innerHTML = "CSIE@CGU";*/
    /*var para = document.getElementById("pid123");
       para.innerHTML = "怎麼那麼棒！！.";*/
- }
+}
  function myFunction() {
  	var btn = document.createElement("BUTTON");
     	btn.innerHTML = "Change this document.";
